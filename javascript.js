5ab2b55aed70bec2da084077200a94bf05180c17
Misha <gimwaydesign@yandex.ru>
2022-01-09 19:53:29 +0300

    Lesson 14 - Loops

--- javascript.js
+++ javascript.js
@@ -1,41 +1,37 @@
 "use strict";
-//Условие
-if (1) {
-        console.log("OK!");
-} else {
-        console.log('Error');     
-}
-const num =50;
+//Циклы
 
-if (num < 50) {
-        console.log('Error');
-} else if (num > 100) {
-        console.log('Mach');
-}else{
-        console.log('OK!');
+//Цикл While будет повторять пока условие будет не равно!
+let num = 50;
+while(num <= 55) {
+        console.log(num);
+        num++;
 }
 
-// Тернарный оператор 
-(num === 50 ) ?   console.log('OK!') :  console.log('Error');
-
-var firstCheck = false,
-    secondCheck = false,
-    access = firstCheck ? "Доступ запрещён" : secondCheck ? "Доступ запрещён" : "Доступ разрешён";
-
-console.log( access ); // выводит в консоль "Доступ разрешён"
-
-const rum = '51';
-switch (rum) {
-        case '49':        
-                console.log('Неверно');
-                break;
-        case '100':
-                console.log('Неверно');
-                break;
-        case '50':
-                console.log('В точку!');
-                break;
-        default:
-                console.log('Не в этот раз');
-                break;
+do { // Сделай пожалуйста что-то
+        console.log(num);
+        num++;
 }
+while (num < 55); // Проверяем условия при помощи цикла While
+
+//Цикл For
+
+let rum = 50;
+ for ( let i = 1; i > 8; i++){
+        console.log(rum);
+        rum++;
+}
+// Цикл прирывается 
+ for ( let i = 1; i < 10; i++){
+       if (i === 6){
+               break;
+       }
+       console.log(i);
+}
+// Значение пропускается в цикле
+for ( let i = 1; i < 10; i++){
+        if (i === 6){
+                continue;
+        }
+        console.log(i);
+ }
