1dca60cb25a64567a2ecf8d184f85b59f042eddd
Misha <gimwaydesign@yandex.ru>
2022-01-11 15:01:20 +0300

    Practice part 2. Apply conditions and cycles

--- javascript.js
+++ javascript.js
@@ -1,37 +1,76 @@
 "use strict";
-//Циклы
+// Practice part 2. Apply conditions and cycles
 
-//Цикл While будет повторять пока условие будет не равно!
-let num = 50;
-while(num <= 55) {
-        console.log(num);
-        num++;
-}
+/* Задание на урок:
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
+str.length - и получить её длину)
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+4) Потренироваться и переписать цикл еще двумя способами*/
 
-do { // Сделай пожалуйста что-то
-        console.log(num);
-        num++;
-}
-while (num < 55); // Проверяем условия при помощи цикла While
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//Цикл For
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
 
-let rum = 50;
- for ( let i = 1; i > 8; i++){
-        console.log(rum);
-        rum++;
+if (personalMovieDB.count <= 10) {
+        console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+} else {
+        console.log("Произошла ошибка");
 }
-// Цикл прирывается 
- for ( let i = 1; i < 10; i++){
-       if (i === 6){
-               break;
-       }
-       console.log(i);
-}
-// Значение пропускается в цикле
-for ( let i = 1; i < 10; i++){
-        if (i === 6){
-                continue;
+// Цикл через While
+/*let i = 0;
+ while (i < 2){ 
+      
+         const a = prompt("Один из последних просмотренных фильмов", ""),
+                b = prompt("На сколько оцените его", "");
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+        } else {
+                console.log('error');
+                i--;
         }
-        console.log(i);
- }
+         i++;
+ }*/
+// Цикл через For
+for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов", ""),
+                b = prompt("На сколько оцените его", "");
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+        } else {
+                console.log('error');
+                i--;
+        }
+}
+// Цикл через do
+/*let i = 0;
+do {
+        const a = prompt("Один из последних просмотренных фильмов", ""),
+                b = prompt("На сколько оцените его", "");
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+        } else {
+                console.log('error');
+                i--;
+        }
+        i++;
+}
+while (i < 2);
+console.log(personalMovieDB);*/
