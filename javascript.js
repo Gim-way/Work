f2ca381294dbaf5c0d77bd99cdf7752656da3ce7
Misha <gimwaydesign@yandex.ru>
2022-01-12 13:00:22 +0300

    Practice part 3. Functions

--- javascript.js
+++ javascript.js
@@ -1,31 +1,80 @@
 "use strict";
-// Lesson number 17. Methods and properties of strings and numbers
-
-const str = 'teSt';
-// console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
-
-let fruit = "Some fruit";
-console.log(fruit.indexOf('q')); // Если -1 то такой кусок стори небыл найден
-console.log(fruit.indexOf('fruit')); // выведет , что начинается с 5 символа 
-
-const logg = 'Hello world';
-console.log(logg.slice(6,11)); // вырежет слово world
-console.log(logg.slice(-5)); // будет вырезать справа налево
-console.log(logg.slice(6)); // вырежет слово world
-console.log(logg.substring(6, 5)); // не подерживает отрицательное значение 
+// Practice part 3. Functions
+/* Задание на урок:
+1) Первую часть задания повторить по уроку
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+P.S. Функции вызывать не обязательно */
 
 
-//Работа с Числами 
+let numberOfFilms;
 
-const num = 12.2;
-console.log(Math.round(num)); // Окпугление числа
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null ||  isNaN(numberOfFilms)) {
+    alert('Ошибка, введите чсило!');
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+  }
+}
+
+start();
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
 
 
-const test = "12.2px";
-console.log(parseInt(test)); // Получает ответ 12 , также он стал числовым
-console.log(parseFloat(test)); // Получаем число 12.2 с плавающим значением 
 
+function rememberMyFilms(){
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
+  }
+}
 
+rememberMyFilms();
 
+function detectPersonalLevel(){
+  if (personalMovieDB.count <= 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+}
+
+detectPersonalLevel();
+
+function  showMyDB(hidden){
+  if (!hidden){ // Значение False передаются сюда и переводится в True
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat); 
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+
+writeYourGenres();
