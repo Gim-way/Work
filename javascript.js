3f181d3b1512a6f5e88af68030f3f72ed0ec4a37
Misha <gimwaydesign@yandex.ru>
2022-01-12 15:20:01 +0300

    Lesson 20. Objects, object destructuring (ES6)

--- javascript.js
+++ javascript.js
@@ -1,25 +1,35 @@
 "use strict";
-// Lesson 19. Callback - function
+// Lesson 20. Objects, object destructuring (ES6)
 
-function first() {
-  //Do something
-  setTimeout(function(){
-    console.log(1);
-  },500);
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border:'black',
+    bg:'red'
+  },
+  age:28,
+  surname: 'Timashov',
+  makeTest: function(){
+    console.log("Test");
+  }
+};
+options.makeTest();
+const {border, bg} = options.colors; // деструктуризация объектов
+console.log(border);
+// console.log(options["colors"]["border"]);
+let counter = 0;
+for (let key in options){
+  if (typeof(options[key])==='object'){
+    for (let i in options[key]){
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    }
+  } else{
+  console.log(`Свойство ${key} имеет значение ${options[key]}`);
+  counter++;
+  }
 }
-function second (){
-  console.log(2);
-}
+console.log(counter);
 
-first();
-second();
-
-
-function learnJS(lang,Callback) {
-  console.log(`Я учу: ${lang}`);
-  Callback();
-}
-function done (){
-  console.log('Я прошёл этот урок!');
-}
-learnJS('JavaScript', done);
+console.log(Object.keys(options).length);
