9ce9b08e2a544672c06e4b0655e50dd9a991c53b
Mikhail Timasov <gimwaydesign@yandex.ru>
2022-01-18 20:21:46 +0300

    // 28. Getting elements from the page

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-//test на новом ноутбуке
+'use strict';
+// 28. Getting elements from the page
+
+const box = document.getElementById('box');
+console.log(box);
+ const btns = document.getElementsByTagName('button'); // в переменную btns плучили псевдомасссив
+ console.log(btns[0]);
+
+ const circles = document.getElementsByClassName('circle');
+
+ console.log(circles);
+
+
+ // Современные методы
+
+ const hearts = document.querySelectorAll('.heart');
+hearts.forEach(item =>  {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('div');
+console.log(oneHeart);
