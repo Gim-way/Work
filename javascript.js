79983f9b4a67bdd881770cc3a4f905ee3c4b470a
Mihkail Timashov <gimwaydesign@yandex.ru>
2022-01-18 11:49:37 +0300

    test №1

---
+++ javascript.js
@@ -0,0 +1,10 @@
+"use strict";
+// Lesson 26 Dynamic typing in JS
+
+
+const a = [1,2,3];
+const b = [1,2,3];
+
+ console.log(a == b);
+ console.log(typeof(+'infinity'));
+ console.log('Ёжик'> 'яблоко');
