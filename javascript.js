1f64616a6ad25286d6398589ac714ab0ea59450b
Misha <gimwaydesign@yandex.ru>
2022-01-19 11:48:45 +0300

    Слияние

--- javascript.js
+++ javascript.js
@@ -1,56 +1,9 @@
 "use strict";
-// Lesson 26 Dynamic typing in JS
+// Lesson 27 Test
 
+let x = 5;
+alert(++x); // ответ 5
 
-// To string
-// 1)
-console.log(typeof(String(null)));
-console.log((String(null)));
-console.log(typeof(String(4)));
+console.log(([]+1+2));
 
-
-//2)   Конкатенация
-console.log(typeof(5 + ''));
-
-const num = 5;
-console.log('https://vk.com/catalog/'+ num);
-
-const fontSize = 26 + 'px';
-
-// To Number
-
-// 1)
-
-console.log(typeof(Number('4')));
-
-//2)
-console.log(typeof(+'5'));// унарный плюс
-
-//3)
-
-console.log(typeof(parseInt('15px', 10)));
-
-let answ = +prompt('Hello', '');
-
-// To boolean
-
-// 0, '',null, NaN, undefined - неправда
-// 1)
-let switcher = null;
-if (switcher) {
-  console.log('Working...');
-}
-
-switcher = 1;
-
-if (switcher) {
-  console.log('Working...');
-}
-
-//2)
-
-console.log(typeof(Boolean('4')));
-
-//3)
-
-console.log(typeof(!!'444444444'));
+alert('1'[0]);
