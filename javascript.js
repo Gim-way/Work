7d408e5e8bb06dd7bb1e1d20ba586530caa65f18
Misha <gimwaydesign@yandex.ru>
2022-01-19 16:16:55 +0300

    29. Actions with messages on the page

--- javascript.js
+++ javascript.js
@@ -1,22 +1,67 @@
 'use strict';
-// 28. Getting elements from the page
+// 29. Actions with messages on the page
 
-const box = document.getElementById('box');
-console.log(box);
- const btns = document.getElementsByTagName('button'); // в переменную btns плучили псевдомасссив
- console.log(btns[0]);
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper');
 
- const circles = document.getElementsByClassName('circle');
+// inLine стили всегда будут в приоритете 
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px ';
 
- console.log(circles);
+box.style.cssText = 'background-color: blue; width: 500px';
 
+btns[1].style.borderRadius = '100%';
+btns[1].style.height =  '80px';
 
- // Современные методы
+// Поменять во всех элементах свойства background
+// for (let i = 0; i< circles.length;i++) {
+//     circles[i].style.backgroundColor = 'red';
+// }
 
- const hearts = document.querySelectorAll('.heart');
-hearts.forEach(item =>  {
-    console.log(item);
+// for (let i = 0; i<h1 hearts.length;i++) {
+//     hearts[i].style.cssText = 'background-color: black; height: 80px;';
+
+// }
+
+// Существуют специальные перебирающие методы 
+
+hearts.forEach(item => {
+    item.style.cssText = 'background-color: black; height: 80px;';
 });
 
-const oneHeart = document.querySelector('div');
-console.log(oneHeart);
+
+const div = document.createElement('div'); // Элемент создан только в  JS
+const text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+// document.body.append(div);
+// document.body.appendChild(div); - старый формат
+
+ wrapper.append(div); // встать в конец объекта    
+//wrapper.prepend(div); // вставть в начале объекта
+// hearts[1].before(div); // перед
+// hearts[0].after(div); // после
+
+//wrapper.insertBefore(div, hearts[0] ); //-вставляю перд первым элементом, устаревший формат
+
+//circles[0].remove(); удалить элемент
+//wrapper.removeChild(hearts[1]); // старый фомат удаления
+
+circles[0].style.backgroundColor = 'red';
+hearts[0].replaceWith(circles[0]); // заменить один элемент на другой 
+
+//wrapper.replaceChild(circles[0], hearts [0]); // старый формат замены
+
+// Ректактирование элемента
+
+div.innerHTML = '<h1>Hello World</h1>'   ; // 1 способ
+//div.textContent = 'Hello'; // 2 способ, работает только с текстом
+ div.style.width = '300px';
+
+div.insertAdjacentHTML('afterend','<h2>Hello</h2>'); // 
+
