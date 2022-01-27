4384ab9882e28c992b0269164802ee3b2c0e26fc
Mikhail Timasov <gimwaydesign@yandex.ru>
2022-01-27 10:51:35 +0300

    //34. Events on mobile devices

--- javascript.js
+++ javascript.js
@@ -1,24 +1,37 @@
 'use strict';
-// 32. DOM Navigation
-/* console.log(document.head);
-console.log(document.documentElement);
-console.log(document.body.childNodes);
-console.log(document.body.firstChild);
-console.log(document.body.firstElementChild);
-console.log(document.body.lastChild);
+//34. Events on mobile devices
+// 1 touchstart - коснулись пальцем
+// 2 touchmove - прикосновение с движением
+// 3 touchend - когда палец убираеться 
+// 4 touchenter - когда ведём пальцем по экрану и наскакиваем на элемент
+// 5 touchleave - ушёл за пределы этого элемента 
+// 6 touchcancel - когда точка соприкосновения не регистрирует
 
-console.log(document.querySelector('#current').parentNode); // получить родитель div.first
-console.log(document.querySelector('#current').parentNode.parentNode);// получаем ещё выше родителя div.wrapper
+// Обработчик событий, чтобы сначало отобразились DOM узлы 
+window.addEventListener('DOMContentLoaded', () =>{
 
+    const box = document.querySelector('.box');
+    box.addEventListener('touchstart', (event) => {
+        event.preventDefault(); // отменяем стандартные дествия браузера 
+        console.log('Start');
+        console.log(event.targetTouches);
 
-console.log(document.querySelector('[data-current = "3"]').previousSibling);
+    });
 
-console.log(document.querySelector('[data-current="3"]').nextElementSibling.outerHTML); // получаем следующий элемент 
-console.log(document.querySelector('#current').parentElement);// получаем элемеyn first
- */
-for (let node  of document.body.childNodes){
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    console.log(node);
-}
+    box.addEventListener('touchmove', (event) => {
+        event.preventDefault(); // отменяем стандартные дествия браузера 
+        console.log(event.targetTouches[0].pageX);
+
+    });
+
+    box.addEventListener('touchend', (event) => {
+        event.preventDefault(); // отменяем стандартные дествия браузера 
+        console.log('end');
+
+    });
+});
+
+// touches - свойство выдает список всех пальцев которые сейчас взаимодейсвуют с экраном
+// targetTouches - все пальцы взаимодействуют с этим элементом
+// changedTouches - список пальцев участвующих в текущем событии
+
