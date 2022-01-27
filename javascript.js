cac9c0b6c6817d305408e0c7f1b0fab2cb2f579d
Mikhail Timasov <gimwaydesign@yandex.ru>
2022-01-27 12:28:32 +0300

    //37. ClassList

--- javascript.js
+++ javascript.js
@@ -1,37 +1,65 @@
 'use strict';
-//34. Events on mobile devices
-// 1 touchstart - коснулись пальцем
-// 2 touchmove - прикосновение с движением
-// 3 touchend - когда палец убираеться 
-// 4 touchenter - когда ведём пальцем по экрану и наскакиваем на элемент
-// 5 touchleave - ушёл за пределы этого элемента 
-// 6 touchcancel - когда точка соприкосновения не регистрирует
+//37. ClassList
+// Экономим память браузера 
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-// Обработчик событий, чтобы сначало отобразились DOM узлы 
-window.addEventListener('DOMContentLoaded', () =>{
+// console.log(btns[0].classList.length);// узнаём количество классов в кнопке 
+console.log(btns[0].classList.item(1)); // показывает название номера класса в кнопке
+console.log(btns[1].classList.add('red', 'second')); // добавляет класс red
+console.log(btns[0].classList.remove('blue')); // удаляет класс blue
+console.log(btns[0].classList.toggle('blue')); 
+// если этот класс присутсвует в элементе, то он его удаляет, 
+//если отсутствует - добавляет
 
-    const box = document.querySelector('.box');
-    box.addEventListener('touchstart', (event) => {
-        event.preventDefault(); // отменяем стандартные дествия браузера 
-        console.log('Start');
-        console.log(event.targetTouches);
+/* Метод - class.List.contains Если класс есть, то возвращает True, а если нет - false */
 
-    });
+if (btns[1].classList.contains('red')) {
+    console.log('red ');
+}
 
-    box.addEventListener('touchmove', (event) => {
-        event.preventDefault(); // отменяем стандартные дествия браузера 
-        console.log(event.targetTouches[0].pageX);
+// addEventListener - назначаеться обратотчик событий
+btns[0].addEventListener('click', () => {
+//  if (!btns[1].classList.contains('red')){
+//      btns[1].classList.add('red');
+//  }else{
+//      btns[1].classList.remove('red');
+//  }
+btns[1].classList.toggle('red');
+}); 
 
-    });
 
-    box.addEventListener('touchend', (event) => {
-        event.preventDefault(); // отменяем стандартные дествия браузера 
-        console.log('end');
+// classname - устарело и не стоит использовать его 
 
-    });
+//console.log(btns[0].className);
+
+
+/* Делегирований событий */
+
+wrapper.addEventListener('click', (event) =>{
+ if (event.target && event.target.tagName == "BUTTON"){
+    console.log('hello');
+ }
 });
 
-// touches - свойство выдает список всех пальцев которые сейчас взаимодейсвуют с экраном
-// targetTouches - все пальцы взаимодействуют с этим элементом
-// changedTouches - список пальцев участвующих в текущем событии
 
+// Продвинутое делегирование
+wrapper.addEventListener('click', (event) =>{
+    if (event.target && event.target.matches('button')){
+       console.log('hello');
+    }
+   });
+   
+
+// делегирование не работает!
+
+/* btns.forEach(btn =>{
+    btn.addEventListener('click', () =>{
+        console.log('hello');
+    });
+}); */ 
+
+
+const btn =document.createElement('button'); 
+btn.classList.add('red');
+wrapper.append(btn);
