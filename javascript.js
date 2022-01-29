d5e18d9d4d6802d4707a7bb3afcda62f26e48689
Mikhail Timasov <gimwaydesign@yandex.ru>
2022-01-29 17:27:59 +0300

    // 39. setTimeout and setInterval

--- javascript.js
+++ javascript.js
@@ -1,65 +1,57 @@
-'use strict';
-//37. ClassList
-// Экономим память браузера 
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
+// 39. Scripts and their execution time. setTimeout and setInterval
+const btn = document.querySelector('.btn');
+let timerId2,
+    i = 0; // глобальная переменная
 
-// console.log(btns[0].classList.length);// узнаём количество классов в кнопке 
-console.log(btns[0].classList.item(1)); // показывает название номера класса в кнопке
-console.log(btns[1].classList.add('red', 'second')); // добавляет класс red
-console.log(btns[0].classList.remove('blue')); // удаляет класс blue
-console.log(btns[0].classList.toggle('blue')); 
-// если этот класс присутсвует в элементе, то он его удаляет, 
-//если отсутствует - добавляет
+// Анимация перемещения куба
 
-/* Метод - class.List.contains Если класс есть, то возвращает True, а если нет - false */
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0; // переменная позиции откуда будет всё стартовать и изменяться
 
-if (btns[1].classList.contains('red')) {
-    console.log('red ');
+    const id = setInterval(frame,10);
+    function frame() {
+        if (pos === 300){
+        /* конечное значение, если будет равно 300, то останавливаем анимацию */
+            clearInterval(id);
+        }else{
+        /* Если аниамация не закончилась */
+            pos++; // начальную позицию увелициваю на еденицу
+            elem.style.top = pos+'px';
+            elem.style.left = pos+'px';
+        }
+    }
 }
 
-// addEventListener - назначаеться обратотчик событий
-btns[0].addEventListener('click', () => {
-//  if (!btns[1].classList.contains('red')){
-//      btns[1].classList.add('red');
-//  }else{
-//      btns[1].classList.remove('red');
-//  }
-btns[1].classList.toggle('red');
-}); 
+
+ btn.addEventListener('click', (myAnimation));
+
+/*  const timerId = setTimeout( function (text){ 
+     // конструкция принимает ту функцию которая будет запускаться через определенное время
+     console.log(text);
+ }, 2000,'Hello'); // мс через 2 скунды получаем 'Hello'
+// трейти аргумент перейдет в функцию  */
 
 
-// classname - устарело и не стоит использовать его 
 
-//console.log(btns[0].className);
-
-
-/* Делегирований событий */
-
-wrapper.addEventListener('click', (event) =>{
- if (event.target && event.target.tagName == "BUTTON"){
-    console.log('hello');
- }
+/* btn.addEventListener('click', ()=>{
+//const timerId2 = setTimeout( logger, 3000);
+ timerId2 = setInterval( logger, 500); // будет выполняться каждые три секунды
 });
 
 
-// Продвинутое делегирование
-wrapper.addEventListener('click', (event) =>{
-    if (event.target && event.target.matches('button')){
-       console.log('hello');
+function logger () {
+    if (i===3){ // когда выполниться функция 3 раза, сработает
+        // Сбросить setTimeout 
+        clearInterval(timerId2);
     }
-   });
-   
+    console.log('text');
+    i++;
+} */
 
-// делегирование не работает!
+// Рекурсивыный вызов setTimeout
 
-/* btns.forEach(btn =>{
-    btn.addEventListener('click', () =>{
-        console.log('hello');
-    });
-}); */ 
-
-
-const btn =document.createElement('button'); 
-btn.classList.add('red');
-wrapper.append(btn);
+/*  let id = setTimeout(function log(){
+    console.log('Hello');
+    id = setTimeout(log,500);
+ },500); */
