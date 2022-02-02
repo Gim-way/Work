1b30653cb372d74b531f6c02dc8b83cbc290df3d
Gim-way <bimbarr777@gmail.com>
2022-02-02 18:58:16 +0300

    // 51. JSON

--- javascript.js
+++ javascript.js
@@ -28,3 +28,37 @@ const clone = JSON.parse(JSON.stringify(persone));
 clone.parents.dad = 'Nike';
 console.log(persone);
 console.log(clone);
+
+let obj = {
+    a: 1,
+    b: 2,
+    second:{
+        c:2,
+        v:8
+    }
+  };
+  let objCopy = Object.assign({}, obj);
+  objCopy.second.c = '99';
+  console.log(obj);
+  console.log(objCopy);
+/* { a: 1, b: 2, second: { c: '99', v: 8 } }
+{ a: 1, b: 2, second: { c: '99', v: 8 } } */
+/* Oblect.assign делает только поверхностую копию */
+
+
+let someObj = {
+    a: 2,
+  };
+  
+  let obj1 = Object.create(someObj, { 
+    b: {
+      value: 2,  
+    },
+    c: {
+      value: 3,
+      enumerable: true,  
+    },
+  });
+  
+  let objCopy1 = Object.assign({}, obj);
+  console.log(objCopy);
